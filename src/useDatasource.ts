47eadd651f90405c5a25553bf4303fb97ad51a75
Alexander Stefański <alex@aps-studio.eu>
2023-07-12 07:52:00 +0200

    v1.0.1

---
+++ src/useDatasource.ts
@@ -0,0 +1,12 @@
+import { EndpointDefinition } from "@nexcodepl/endpoint-types";
+import { useRefStatic } from "@nexcodepl/store-react";
+import { Datasource, DatasourceConfig } from "@nexcodepl/endpoint-client";
+
+export function useDatasource<TEndpoint extends EndpointDefinition<any, any, any, boolean>>(
+    endpointDefinition: TEndpoint,
+    config?: DatasourceConfig
+): Datasource<TEndpoint> {
+    const ds = useRefStatic<Datasource<TEndpoint>>(() => new Datasource<TEndpoint>(endpointDefinition, config ?? {}));
+
+    return ds.current;
+}
